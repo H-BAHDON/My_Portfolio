f3b3d0c7c9b38bc630f58bafa0437c9aa7dd62aa
Hussein Bahdon <husseinbahdon1@gmail.com>
2024-01-14 15:07:49 +0000

    Updated github

---
+++ src/components/github/Github.js
@@ -0,0 +1,24 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar";
+import { Row, Col } from "react-bootstrap";
+
+function Github() {
+  return (
+    <Row className="justify-content-center" style={{ marginRight: 0 }}>
+      <Col xs={12} md={8} lg={6}>
+        <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
+          Days I <strong className="grey">Code</strong>
+        </h1>
+        <GitHubCalendar
+          username="H-BAHDON"
+          blockSize={8}
+          blockMargin={7}
+          color="#c084f5"
+          fontSize={16}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default Github;
